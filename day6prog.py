d7cd5340ca46f989266844e3968018abf435bf53
m4n1g <manimaran990@gmail.com>
2020-12-06 13:56:08 -0500

    day6 part2

--- day6prog.py
+++ day6prog.py
@@ -1,4 +1,4 @@
-def count_yes(fname):
+def count_yes(fname): 
 	ans_lst = []
 	with open(fname) as f:
 		tmp = [] 
@@ -10,7 +10,9 @@ def count_yes(fname):
 				tmp.append(set(line.strip()))
 		ans_lst.append(tmp)
 
-	return sum([ len(set([ item for sublist in list_item for item in sublist ])) for list_item in ans_lst ])
+	print(f"part 1: {sum([ len(set([ item for sublist in list_item for item in sublist ])) for list_item in ans_lst ])}")
+	print(f"part 2: {sum([ len(set.intersection(*i)) for i in [l for l in ans_lst] ])}")
+
 
 
 print(count_yes('day6inp.txt'))
